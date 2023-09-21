991054491075e4f600aad063e979abaf337a0bc1
matthxu <128595283+matthxu@users.noreply.github.com>
2023-09-21 23:03:44 +1000

    Fixed minor bugs and cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-    /*----- constants -----*/
+/*----- constants -----*/
 const result = {
-    correct : ['Nice!', 'Well Done!', 'Perfect!', "That's it!", "Great work!"],
-    incorrect : ['Not quite!', 'Try Again!', 'Oops!', 'Keep Trying!', 'So close', 'You can do it!' ]
-}
-const gameComplete = false;
+    correct: ["Nice!", "Well Done!", "Perfect!", "That's it!", "Great work!"],
+    incorrect: [
+        "Not quite!",
+        "Try Again!",
+        "Oops!",
+        "Keep Trying!",
+        "So close",
+        "You can do it!",
+    ],
+};
 
 /*----- state variables -----*/
 let startingGuesses = 5;
@@ -11,47 +17,44 @@ let guessCount = 5;
 let count = 0;
 let firstCard;
 let newGuesses = 0;
-
+let gameComplete = false;
 
 /*----- cached elements  -----*/
-const cards = document.querySelectorAll('.card_face');
-const remainingGuess = document.querySelector('.guess_count');
-const results = document.querySelector('.result');
+const cards = document.querySelectorAll(".card_face");
+const remainingGuess = document.querySelector(".guess_count");
+const results = document.querySelector(".result");
 
-const images = document.querySelectorAll('img');
-const cardBacks = document.querySelectorAll('.card_face_back');
+const images = document.querySelectorAll("img");
+const cardBacks = document.querySelectorAll(".card_face_back");
 
-const playAgain = document.getElementById('play_again');
-const shuffleBtn = document.getElementById('shuffle');
+const playAgain = document.getElementById("play_again");
+const shuffleBtn = document.getElementById("shuffle");
 
-const changeDifficulty = document.querySelector('.show');
-const guessOptions = document.querySelector('.guess_options');
-const changeAmount = document.querySelector('.user_guesses');
-const changeBtn = document.querySelector('.change_guess');
+const changeDifficulty = document.querySelector(".show");
+const guessOptions = document.querySelector(".guess_options");
+const changeAmount = document.querySelector(".user_guesses");
+const changeBtn = document.querySelector(".change_guess");
 
-const artNote = document.getElementById('art');
+const artNote = document.getElementById("art");
 
 /*----- event listeners -----*/
-cards.forEach(card => card.addEventListener('click', handleClick))
-playAgain.addEventListener('click', initialise);
-shuffleBtn.addEventListener('click', initialise);
-changeDifficulty.addEventListener('click', revealOptions);
-changeBtn.addEventListener('click', changeGuess)
-
+cards.forEach((card) => card.addEventListener("click", handleClick));
+playAgain.addEventListener("click", initialise);
+shuffleBtn.addEventListener("click", initialise);
+changeDifficulty.addEventListener("click", revealOptions);
+changeBtn.addEventListener("click", changeGuess);
 
 /*----- functions -----*/
 
-function initialise() { 
+function initialise() {
     hideCards();
     shuffle();
     resetGuesses();
-    // timer();
     results.innerHTML = "Click any card to get started!";
-    // shuffleBtn.style.visibility = 'visible';
     changeDifficulty.style.visibility = "none";
-    playAgain.style.display = 'none';
-    artNote.style.visibility = 'hidden';
-    guessOptions.style.visibility = 'hidden';
+    playAgain.style.display = "none";
+    artNote.style.visibility = "hidden";
+    guessOptions.style.visibility = "hidden";
     gameComplete = false;
 }
 
@@ -60,25 +63,24 @@ function resetGuesses() {
         guessCount = startingGuesses;
     } else guessCount = newGuesses;
     remainingGuess.innerHTML = `GUESSES LEFT: <strong>${guessCount}</strong>`;
-    remainingGuess.style.visibility = 'visible';
+    remainingGuess.style.visibility = "visible";
 }
 
-
-
 function handleClick() {
+    // Prevents shuffle button from being visible on endscreen
     if (gameComplete) return;
-    
+
     // Reveal shuffle button after first turn is taken
     if (guessCount < startingGuesses) {
-        shuffleBtn.style.display = 'inline';
+        shuffleBtn.style.display = "inline";
     }
 
     // Prevent repeat selection
     if (this.classList[2] != undefined || guessCount === 0) return;
     // Flip user selected card
-    this.classList.toggle('is-flipped');
+    this.classList.toggle("is-flipped");
     count += 1;
-    
+
     if (count === 2) {
         count = 0;
         const secondCard = this.classList;
@@ -93,152 +95,120 @@ function handleClick() {
             encourage();
             setTimeout(resetSelectedCards, 600);
             return;
-            function resetSelectedCards () {
-                firstCard.remove('is-flipped');
-                secondCard.remove('is-flipped');
+            function resetSelectedCards() {
+                firstCard.remove("is-flipped");
+                secondCard.remove("is-flipped");
                 return;
             }
-            
         }
     }
     firstCard = this.classList;
 }
 
-function updateGuessCount (){
+function updateGuessCount() {
     guessCount -= 1;
     guessString = guessCount.toString();
     remainingGuess.innerHTML = `GUESSES LEFT: <strong>${guessString}</strong>`;
     if (guessCount === 0) {
         gameOver();
-}}
-
+    }
+}
 
 const cardsArr = Array.from(cards);
 const imagesArr = Array.from(images);
 
 function shuffle() {
-    shuffleBtn.style.display = 'none';
+    shuffleBtn.style.display = "none";
     // Images and card pairs are randomised together using the same randomised index arr
     let randomCardsArr = [];
     let randomImageArr = [];
     let randomIndex = [];
-    
+
     cardsArr.forEach((card, index) => {
-        randomIndex.push(Math.floor(Math.random()*16));
-        randomCardsArr.splice([randomIndex[index]],0,(card.classList[1]));
-    })
-    imagesArr.forEach((image, index) => {
-        randomImageArr.splice([randomIndex[index]],0,(image));
+        randomIndex.push(Math.floor(Math.random() * 16));
+        randomCardsArr.splice([randomIndex[index]], 0, card.classList[1]);
     });
-    
+    imagesArr.forEach((image, index) => {
+        randomImageArr.splice([randomIndex[index]], 0, image);
+    });
+
     cardsArr.forEach((card, index) => {
         card.classList.remove(card.classList[1]);
         card.classList.add(randomCardsArr[index]);
-    })
-    imagesArr.forEach((image,index) => {
+    });
+    imagesArr.forEach((image, index) => {
         cardBacks[index].appendChild(randomImageArr[index]);
-    })
+    });
 }
 
 function gameWon() {
     let checkArr = [];
     let playerWon;
-    cards.forEach(card => {
-        checkArr.push(card.classList.contains('is-flipped'))
-        playerWon = checkArr.every(element => element === true);
-    })
+    cards.forEach((card) => {
+        checkArr.push(card.classList.contains("is-flipped"));
+        playerWon = checkArr.every((element) => element === true);
+    });
     if (playerWon) {
+        results.innerHTML = "You did it!";
+        playAgain.style.display = "inline";
+        artNote.style.visibility = "visible";
+        shuffleBtn.style.display = "none";
+        remainingGuess.style.visibility = "hidden";
+        changeDifficulty.style.visibility = "hidden";
         gameComplete = true;
-        results.innerHTML = 'You did it!';
-        playAgain.style.display = 'inline';
-        // high score  
-        artNote.style.visibility = 'visible';
-        shuffleBtn.style.display = 'none';
-        remainingGuess.style.visibility = 'hidden';
-        changeDifficulty.style.visibility = 'hidden';
     }
 }
 function celebrate() {
-    results.innerHTML = result.correct[Math.floor(Math.random()*4)]
+    results.innerHTML = result.correct[Math.floor(Math.random() * 4)];
 }
 
 function gameOver() {
+    results.innerHTML = "Better luck next time!";
     showCards();
     showHidden();
-    shuffleBtn.style.display = 'none';
-    results.innerHTML = 'Better luck next time!';
+    shuffleBtn.style.display = "none";
     gameComplete = true;
 }
 function encourage() {
-    results.innerHTML = result.incorrect[Math.floor(Math.random()*4)]
+    results.innerHTML = result.incorrect[Math.floor(Math.random() * 4)];
 }
 
 function hideCards() {
-    cards.forEach(card => {
-        card.classList.remove('is-flipped');
-    }
-)}
+    cards.forEach((card) => {
+        card.classList.remove("is-flipped");
+    });
+}
 function showCards() {
     setTimeout(showAll, 800);
-    function showAll () {
-        cards.forEach(card => {
-        card.classList.add('is-flipped');
+    function showAll() {
+        cards.forEach((card) => {
+            card.classList.add("is-flipped");
+        });
     }
-)}
 }
-
 function showHidden() {
-    playAgain.style.display = 'inline';
-    changeDifficulty.style.visibility = 'visible';
-    remainingGuess.style.visibility = 'hidden';
+    playAgain.style.display = "inline";
+    changeDifficulty.style.visibility = "visible";
+    remainingGuess.style.visibility = "hidden";
 }
 
 function revealOptions() {
-    guessOptions.style.visibility = 'visible';
-    playAgain.style.display='none';
-    changeDifficulty.style.visibility = 'hidden';
+    guessOptions.style.visibility = "visible";
+    playAgain.style.display = "none";
+    changeDifficulty.style.visibility = "hidden";
 }
 
 function changeGuess() {
+    changeDifficulty.style.visibility = "hidden";
     newGuesses = changeAmount.value;
+    // Default to 25 if no user input to new guess limit
+    if (newGuesses === "") {
+        newGuesses = 25;
+    }
     guessCount = newGuesses;
-    // playAgain.style.visibility = 'hidden';
-    changeDifficulty.style.visibility = 'hidden';
-    guessOptions.style.visibility = 'hidden';
+    // Hide guess-changing elements
+    guessOptions.style.visibility = "hidden";
     initialise();
 }
 
 initialise();
-
-
-
-
-
-
-
-
-
-// const timerTxt = document.getElementById('timer');
-
-// function timer() {
-    // let sec = 0;
-    // let min = 0;
-    // let startTime = 0;
-    // let elapsedTime = 0;
-//     startTime = Date.now() - elapsedTime;
-//     intervalId = setInterval(updateTime, 1000)
-
-//     function updateTime() {
-//         elapsedTime = Date.now() - startTime;
-
-//         sec = Math.floor((elapsedTime / 1000) % 60);
-//         min = Math.floor((elapsedTime / (1000 * 60)) % 60);
-
-//         sec = pad(sec);
-//         min = pad(min);
-//         timerTxt.innerHTML = `${min} : ${sec}`;
-//         function pad(unit) {
-//             return (('0') + unit).length > 2 ? unit : "0" + unit;
-//         }
-//     }
-// }
